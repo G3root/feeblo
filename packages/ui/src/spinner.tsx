6edccf6d7624a4ba1a86f3d34981349fb1d06c33
nafees nazik <nafeesnazik21@gmail.com>
2026-06-11 10:10:42 +0530

    refactor: update Spinner component to wrap HugeiconsIcon in a div for improved structure

--- packages/ui/src/spinner.tsx
+++ packages/ui/src/spinner.tsx
@@ -2,16 +2,21 @@ import { Loading03Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { cn } from "./utils";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({
+  className,
+  ...props
+}: Omit<React.ComponentProps<typeof HugeiconsIcon>, "icon">) {
   return (
-    <HugeiconsIcon
-      aria-label="Loading"
-      className={cn("size-4 animate-spin", className)}
-      icon={Loading03Icon}
-      role="status"
-      strokeWidth={2}
-      {...props}
-    />
+    <div className="animate-spin">
+      <HugeiconsIcon
+        aria-label="Loading"
+        className={cn("size-4", className)}
+        icon={Loading03Icon}
+        role="status"
+        strokeWidth={2}
+        {...props}
+      />
+    </div>
   );
 }
 
