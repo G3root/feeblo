cef3a0c42bef77e52da0439d272ec5686d8883f6
nafees nazik <nafeesnazik21@gmail.com>
2026-07-08 22:03:16 +0530

    feat: enable WidgetApiLive in HTTP routing

--- packages/domain/src/http/router.ts
+++ packages/domain/src/http/router.ts
@@ -6,7 +6,7 @@ import { AuthApiLive } from "../auth/api-live";
 import { MediaApiLive } from "../media/api-live";
 import { OrganizationApiLive } from "../organization/api-live";
 import { ProfileApiLive } from "../profile/api-live";
-// import { WidgetApiLive } from "../widget/api-live";
+import { WidgetApiLive } from "../widget/api-live";
 import { Api } from "./api";
 
 export const HttpRoute = HttpApiBuilder.layer(Api, {
@@ -15,6 +15,6 @@ export const HttpRoute = HttpApiBuilder.layer(Api, {
   Layer.provide(AuthApiLive),
   Layer.provide(MediaApiLive),
   Layer.provide(OrganizationApiLive),
-  Layer.provide(ProfileApiLive)
-  // Layer.provide(WidgetApiLive)
+  Layer.provide(ProfileApiLive),
+  Layer.provide(WidgetApiLive)
 );
