7a08c0d197d0ee0a56246a8041c24e0d16102fc3
nafees nazik <nafeesnazik21@gmail.com>
2026-08-05 09:32:43 +0530

    fix: unnecessary conversition

--- packages/ui/src/markdown-content.tsx
+++ packages/ui/src/markdown-content.tsx
@@ -1,6 +1,4 @@
-// import "@feeblo/web-shared/typeset.css";
-import { markdownToHtml } from "@feeblo/utils/markdown";
-import { type ComponentProps, useMemo } from "react";
+import type { ComponentProps } from "react";
 import { cn } from "./utils";
 
 type MarkdownContentProps = Omit<
@@ -15,13 +13,11 @@ export function MarkdownContent({
   content,
   ...props
 }: MarkdownContentProps) {
-  const html = useMemo(() => markdownToHtml(content), [content]);
-
   return (
     <div
-      className={cn("typeset whitespace-pre-wrap", className)}
+      className={cn("typeset", className)}
       // biome-ignore lint/security/noDangerouslySetInnerHtml: content is sanitized Markdown rendered to HTML
-      dangerouslySetInnerHTML={{ __html: html }}
+      dangerouslySetInnerHTML={{ __html: content }}
       {...props}
     />
   );
