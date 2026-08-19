ec12c853458250dfb865714a9508e35c91b257ff
nafees nazik <nafeesnazik21@gmail.com>
2026-08-19 14:05:01 +0530

    chore: add remark

--- packages/utils/src/markdown/index.ts
+++ packages/utils/src/markdown/index.ts
@@ -11,11 +11,13 @@
 import rehypeParse from "rehype-parse";
 import rehypeRemark from "rehype-remark";
 import remarkGfm from "remark-gfm";
-import remarkHtml from "remark-html";
 import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
 import remarkStringify from "remark-stringify";
+import rehypeStringify from "rehype-stringify";
 import { unified } from "unified";
 
+import { rehypeCodeHighlight } from "./rehype-code-highlight";
 import { rehypeJoinParagraph } from "./rehype-join-paragraph";
 import { customBreakHandler } from "./remark-break-handler";
 
@@ -42,7 +44,9 @@ export const markdownToHtml = (markdown: string): string => {
   return unified()
     .use(remarkParse)
     .use(remarkGfm)
-    .use(remarkHtml)
+    .use(remarkRehype)
+    .use(rehypeCodeHighlight)
+    .use(rehypeStringify)
     .processSync(markdown)
     .toString();
 };
