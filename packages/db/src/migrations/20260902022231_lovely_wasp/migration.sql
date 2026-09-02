0f72db6d50ea284c1cbf1a2df419d265c763fd82
nafees nazik <nafeesnazik21@gmail.com>
2026-09-02 16:57:10 +0530

    chore: regenerate migration

--- packages/db/src/migrations/20260902022231_lovely_wasp/migration.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "comment" ADD COLUMN "status_update_type" text;--> statement-breakpoint
--- packages/db/src/migrations/20260902024042_acoustic_luke_cage/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "comment" ADD COLUMN "status_update_id" text;--> statement-breakpoint
-ALTER TABLE "comment" DROP COLUMN "status_update_type";--> statement-breakpoint
-ALTER TABLE "comment" ADD CONSTRAINT "comment_status_update_id_post_status_id_fkey" FOREIGN KEY ("status_update_id") REFERENCES "post_status"("id") ON DELETE SET NULL;
