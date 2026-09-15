7f689fa8294825ee41c39e1824e8b8ed7949abbd
nafees nazik <nafeesnazik21@gmail.com>
2026-09-16 00:44:45 +0530

    chore: fix migration

--- packages/db/src/migrations/20260915093307_polite_vapor/migration.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "comment" ADD COLUMN "merged_from_post_id" text;--> statement-breakpoint
-ALTER TABLE "upvote" ADD COLUMN "merged_from_post_id" text;--> statement-breakpoint
-CREATE INDEX "comment_mergedFromPostId_idx" ON "comment" ("merged_from_post_id");--> statement-breakpoint
-CREATE INDEX "upvote_mergedFromPostId_idx" ON "upvote" ("merged_from_post_id");--> statement-breakpoint
-ALTER TABLE "comment" ADD CONSTRAINT "comment_merged_from_post_id_post_id_fkey" FOREIGN KEY ("merged_from_post_id") REFERENCES "post"("id") ON DELETE SET NULL;--> statement-breakpoint
-ALTER TABLE "upvote" ADD CONSTRAINT "upvote_merged_from_post_id_post_id_fkey" FOREIGN KEY ("merged_from_post_id") REFERENCES "post"("id") ON DELETE SET NULL;
